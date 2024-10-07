5308d67842b1461dc436991ba6ce61211346a71e
kae-chi <kechi@bu.edu>
2024-10-07 17:05:04 -0400

    arduino code

---
+++ charge_box.cpp/charge_box.cpp.ino
@@ -0,0 +1,57 @@
+#include <SPI.h>
+#include <SD.h>
+
+//define relevant pins
+
+//countdown red LED
+#define countDown 2
+
+//charge pin
+
+#define charge 11
+
+
+
+// global vars
+
+
+unsigned long currentTime;
+unsigned long beginningTime;
+unsigned long lightupTime;
+int countdown = 15;
+
+
+
+
+void setup() {
+
+  pinMode(countDown, OUTPUT);
+  digitalWrite(countDown, LOW);
+
+  pinMode(charge, OUTPUT);
+  digitalWrite(charge, LOW);
+
+
+
+
+  beginningTime = millis();
+  lightupTime = beginningTime + countdown;
+}
+
+void loop() {
+  currentTime = millis();
+
+  for (int i = 0; i < countdown; i++) {
+
+     unsigned long desiredTime = currentTime + i;
+    if (desiredTime >= currentTime) {
+      digitalWrite(countDown, HIGH);
+    }
+    digitalWrite(countDown, LOW);
+  }
+
+  if (currentTime >= lightupTime) {
+    digitalWrite(countDown, HIGH);
+    digitalWrite(charge, HIGH);
+  }
+}
